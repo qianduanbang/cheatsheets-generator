62b6d4118eaa00976271f6a66e6133a38917f54f
MwumLi <MwumLi@hotmail.com>
2017-04-10 16:18:02 +0800

    add CNAME support for gh-pages

--- build/gh-pages.js
+++ build/gh-pages.js
@@ -1,11 +1,19 @@
 var ghpages = require('gh-pages');
+var fs = require('fs');
 var path = require('path');
 var package = require('../package.json');
 var utils = require('./utils');
 
 
 var repo = utils.param(package, 'config.gh-pages.git', 'https://github.com/MwumLi/simple-cheatsheets.git');
-ghpages.publish(path.join(__dirname, '../dist'), {
+var dist = path.join(__dirname, '../dist');
+var cname = utils.param(package, 'config.gh-pages.cname');
+
+if (cname) {
+  fs.writeFileSync(path.join(dist, 'CNAME'), cname, 'utf-8');
+}
+
+ghpages.publish(dist, {
   repo: repo,
   message: utils.param(package, 'config.gh-pages.message', 'updated'),
   user: {
